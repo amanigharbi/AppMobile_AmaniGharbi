00facb3de6549b8ff4d0a34fb83a9a8e9964a7f9
ameni <amanigharbi9@gmail.com>
2022-11-26 20:11:25 +0100

    integrate patch api

--- app/src/main/java/com/example/appmobile_amanigharbi/ui/profile/EditProfileFragment.java
+++ app/src/main/java/com/example/appmobile_amanigharbi/ui/profile/EditProfileFragment.java
@@ -2,6 +2,7 @@ package com.example.appmobile_amanigharbi.ui.profile;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,27 +13,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.appmobile_amanigharbi.R;
 import android.app.DatePickerDialog;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
 public class EditProfileFragment extends Fragment {
     private EditText dateEdt;
+    private EditText FirstName;
+    private EditText LastName;
+    private RadioButton Genre1;
+    private RadioButton Genre2;
+    private RadioButton Genre3;
+    private Button btnEdit;
     private EditProfileViewModel mViewModel;
+    private String genre ="M";
+
 
     public static EditProfileFragment newInstance() {
         return new EditProfileFragment();
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_edit_profile, container, false);
 // on below line we are initializing our variables.
         dateEdt = (EditText) view.findViewById(R.id.birth);
-
+        FirstName = (EditText) view.findViewById(R.id.prenom);
+        LastName = (EditText) view.findViewById(R.id.nom);
+        Genre1 = (RadioButton) view.findViewById(R.id.radioButton);
+        Genre2 = (RadioButton) view.findViewById(R.id.radioButton2);
+        Genre3 = (RadioButton) view.findViewById(R.id.radioButton3);
+        btnEdit =(Button) view.findViewById(R.id.btn_edit);
         // on below line we are adding click listener
         // for our pick date button
         dateEdt.setOnClickListener(new View.OnClickListener() {
@@ -69,10 +99,64 @@ public class EditProfileFragment extends Fragment {
                 datePickerDialog.show();
             }
         });
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking if the edit text is empty or not.
+                if ((FirstName.getText().toString().isEmpty()) && LastName.getText().toString().isEmpty()) {
+                    // displaying a toast message if the edit text is empty.
+                    Toast.makeText(getContext(), "Please enter your data..", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // calling a method to update data in our API.
+                if (Genre1.isChecked()){
+                    genre="M";
+                }
+                if (Genre2.isChecked()){
+                    genre="F";
+                }
+                if (Genre3.isChecked()){
+                    genre="A";
+                }
+
+                callPUTDataMethod(FirstName.getText().toString(), LastName.getText().toString(),genre,dateEdt.getText().toString());
+            }
+        });
         return view;
     }
-
-    @Override
+    private void callPUTDataMethod(String FirstName, String LastName,String genre,String birth) {
+        String url = "http://api.shield.kaisens.fr/api/child/639/";
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        StringRequest request = new StringRequest(Request.Method.PATCH, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getContext(), "Data Updated..", Toast.LENGTH_SHORT).show();
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String output = jsonObject.getString("first_name") + "\n" + jsonObject.getString("last_name") + "\n" + jsonObject.getString("birthday") + "\n" + jsonObject.getString("gender");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Fail to update data..", Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("first_name", FirstName);
+                params.put("last_name", LastName);
+                params.put("birthday", dateEdt.toString());
+                params.put("gender", genre);
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+            @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(EditProfileViewModel.class);
--- app/src/main/java/com/example/appmobile_amanigharbi/ui/profile/MyRvAdapter.java
+++ app/src/main/java/com/example/appmobile_amanigharbi/ui/profile/MyRvAdapter.java
@@ -1,12 +1,16 @@
 package com.example.appmobile_amanigharbi.ui.profile;
 
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.appmobile_amanigharbi.R;
@@ -44,11 +48,23 @@ class MyRvAdapter extends RecyclerView.Adapter<MyRvAdapter.MyHolder> {
     class MyHolder extends RecyclerView.ViewHolder {
         TextView tvNom;
         TextView tvAge;
+        CardView cardView;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
             tvNom = itemView.findViewById(R.id.NomEnfant);
             tvAge = itemView.findViewById(R.id.AgeEnfant);
+            cardView =  (CardView) itemView.findViewById(R.id.card1);
+            cardView.setOnClickListener(this::onClick);
         }
-    }
+
+        public void onClick(View v)
+        {
+            Intent intent = new Intent(v.getContext(),EditProfileFragment.class);
+            intent.putExtra("message", "hh");
+            v.getContext().startActivity(intent);
+        }
+
+        }
+
 
 }
