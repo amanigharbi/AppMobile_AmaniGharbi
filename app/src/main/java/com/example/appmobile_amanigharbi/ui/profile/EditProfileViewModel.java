7c17cfde01d489a6a6d77fe17116efa9538e0297
ameni <amanigharbi9@gmail.com>
2022-11-26 01:09:37 +0100

    design v1

---
+++ app/src/main/java/com/example/appmobile_amanigharbi/ui/profile/EditProfileViewModel.java
@@ -0,0 +1,7 @@
+package com.example.appmobile_amanigharbi.ui.profile;
+
+import androidx.lifecycle.ViewModel;
+
+public class EditProfileViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
